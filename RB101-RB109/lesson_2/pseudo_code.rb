b1a491a3c9bd844ea807cb875c5abc5942d361e4
tlgakn <tolgaakin737@icloud.com>
2020-07-04 17:53:30 +0200

    lesson 2

---
+++ RB101-RB109/lesson_2/pseudo_code.rb
@@ -0,0 +1,15 @@
+def find_greatest(numbers)
+  saved_number = nil
+
+  numbers.each do |num|
+    saved_number ||= num  # assign to first value
+    if saved_number >= num
+      next
+    else
+      saved_number = num
+    end
+  end
+
+  saved_number
+end
+
